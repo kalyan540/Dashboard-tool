86ea7a43dacf827ce115ad3e14baeb5d95784da7
Kalyan540 <kalyanroczzzz@gmail.com>
2024-12-16 15:17:49 +0530

    Added: Publish/Draft Button

--- superset-frontend/src/components/PageHeaderWithActions/index.tsx
+++ superset-frontend/src/components/PageHeaderWithActions/index.tsx
@@ -147,7 +147,7 @@ export const PageHeaderWithActions = ({
               <CertifiedBadge {...certificatiedBadgeProps} />
             )}
             {showFaveStar && <FaveStar {...faveStarProps} />}
-            {/*titlePanelAdditionalItems*/}
+            {titlePanelAdditionalItems[0]}
           </div>
         )}
         {console.log(CertifiedBadge, FaveStar, buttonsStyles, titlePanelAdditionalItems)}

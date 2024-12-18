3919a0241fa4a92f29459a056f642694f3e4e6ad
Kalyan540 <kalyanroczzzz@gmail.com>
2024-12-18 17:32:07 +0530

    fixed : issue

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import React from 'react';
 import { t } from '@superset-ui/core';
 import {
   ControlPanelConfig,

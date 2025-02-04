d513ac0d7fe8c9e2a156bfdb49cc98e79ba8571c
Kalyan540 <kalyanroczzzz@gmail.com>
2025-02-04 14:10:24 +0530

    fix: update baselink and href for table model view in initialization

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -1191,7 +1191,7 @@ class SqlaTable(
     normalize_columns = Column(Boolean, default=False)
     always_filter_main_dttm = Column(Boolean, default=False)
 
-    baselink = "kalyan"
+    baselink = "tablemodelview"
 
     export_fields = [
         "table_name",

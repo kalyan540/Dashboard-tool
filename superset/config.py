531549c3e3331b7c22e8380ddb44c0741d45fe30
tkalyan <kalyanroczzzz@gmail.com>
2025-03-10 12:25:13 +0530

    Commented out frame-ancestors in content security policy

--- superset/config.py
+++ superset/config.py
@@ -1660,7 +1660,7 @@ TALISMAN_DEV_CONFIG = {
             "'unsafe-inline'",
         ],
         "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        "frame-ancestors": ["'self'", "http://localhost:3000", "http://localhost:3002", "http://localhost:5000"],
+        #"frame-ancestors": ["'self'", "http://localhost:3000", "http://localhost:3002", "http://localhost:5000"],
     },
     "content_security_policy_nonce_in": ["script-src"],
     "force_https": False,
